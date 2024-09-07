3d3cf55fb9a250f837f871cfbe28dbc00a22b20f
ari <ariel.leiva@mi.unc.edu.ar>
2024-09-06 21:50:27 -0300

    Hasta el 8 hecho

--- Script.sql
+++ Script.sql
@@ -8,6 +8,7 @@ INNER JOIN
 			AS cityCountry 
 ON cityCountry.Code = city.CountryCode;
 
+
 # 2.
 WITH my_avg (value) AS (
 	SELECT AVG(Population)
@@ -27,6 +28,7 @@ FROM city
 INNER JOIN my_avg
 ON city.Population > my_avg.value;
 
+
 # 3.
 WITH asi_country (Population) AS (
 	SELECT DISTINCT Population
@@ -45,6 +47,7 @@ WHERE city.Population >= SOME (SELECT DISTINCT Population
 	FROM country
 	WHERE country.Continent = 'Asia');
 
+
 # 4.
 SELECT country.Name, cl.Language
 FROM country
@@ -55,6 +58,66 @@ WHERE cl.Percentage > ALL (
 		FROM countrylanguage as cl_official
 		WHERE (cl_official.IsOfficial = 'T' AND country.Code = cl_official.CountryCode) 
 	);
-	
-# 5.
+
+
+# Comando útil: 
+DESCRIBE country;
+# 5. Con subquery
 SELECT DISTINCT country.Region
+FROM country
+INNER JOIN city
+ON city.CountryCode = country.Code 
+WHERE country.SurfaceArea < 1000 AND EXISTS (
+		  SELECT city.Name
+		  FROM city
+		  WHERE	(city.CountryCode = country.Code AND city.Population > 100000)
+	  );
+
+# 5. Sin subquery. OBSERVACIÓN: Sirve bastante tener en cuenta la correspondencia de cardinalidades.
+SELECT DISTINCT country.Region
+FROM city
+INNER JOIN country
+ON (country.Code = city.CountryCode AND country.SurfaceArea < 1000 AND city.Population > 100000);
+
+# Otra observación: El INNER JOIN es "simétrico"
+SELECT DISTINCT country.Region
+FROM country
+INNER JOIN city
+ON (country.Code = city.CountryCode AND country.SurfaceArea < 1000 AND city.Population > 100000);
+
+
+# 6.
+SELECT country.Name, city.Name, city.Population
+FROM city
+INNER JOIN country
+ON (city.CountryCode = country.Code)
+WHERE city.Population = (
+	SELECT MAX(country_city.Population)
+	FROM city AS country_city
+	WHERE country_city.CountryCode = country.Code
+	LIMIT 1
+);
+
+# AYUDAAAAAAa SELECT country.Name, city.Population
+#FROM country; 
+
+#SELECT city.CountryCode, MAX(city.Population)
+#FROM city
+#GROUP BY city.Population;
+
+# 7.
+SELECT country.Name, countrylanguage.Language
+FROM country
+INNER JOIN countrylanguage
+ON (countrylanguage.CountryCode = country.Code AND countrylanguage.IsOfficial != 'T')
+WHERE countrylanguage.Percentage > (
+	SELECT AVG(countrylanguage.Percentage)
+	FROM countrylanguage
+	WHERE (countrylanguage.CountryCode = country.Code AND countrylanguage.IsOfficial = 'T')
+	LIMIT 1
+);
+
+# 8.
+SELECT country.Region, SUM(Population) as Population
+FROM country
+GROUP BY country.Region;
